31e444f0404ef87eb4fc6b2e165b13422c974514
Matteo Depalo <matteodepalo@gmail.com>
2015-02-23 17:27:08 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-timecop'
+  name: 'ember-cli-timecop',
+
+  treeFor: function(name) {
+    if (name !== 'vendor') { return; }
+
+    return this.treeGenerator(path.join(__dirname, 'node_modules'));
+  },
+
+  included: function(app) {
+    this._super.included(app);
+
+    if (app.env !== 'production') {
+      this.app.import('vendor/timecop/timecop.js');
+    }
+  }
 };
